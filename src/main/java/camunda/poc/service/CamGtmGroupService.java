9b0225bc255131ea56d4d7604564f9781c080bb2
Khafizov Airat <airat85@bk.ru>
2022-04-05 16:20:46 +0300

    13239 .gitignore

--- src/main/java/camunda/poc/service/CamGtmGroupService.java
+++ src/main/java/camunda/poc/service/CamGtmGroupService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
